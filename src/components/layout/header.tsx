952fb0207ef504de8734a4fdeb5745d1550106eb
Rich Williams <rich@toneworm.com>
2026-01-01 16:20:59 +0000

    fixed header linting error

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,16 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="px-4 container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
-          <a className="mr-4 flex items-center gap-2 lg:mr-6" href="/">
-            <span className="font-bold">Three.js Projects</span>
-          </a>
+          <Link href="/">
+            <a className="mr-4 flex items-center gap-2 lg:mr-6">
+              <span className="font-bold">Three.js Projects</span>
+            </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-end gap-2">
           <ThemeToggle />
